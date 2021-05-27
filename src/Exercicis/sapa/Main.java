ffc8c42fea87e9aaa6869c57b975347cb7ed18f2
AlexGrabulosaPuertas <a.grabulosa@sapalomera.cat>
2021-05-27 09:11:43 +0200

    printat "Hello developers!"

--- src/Exercicis/sapa/Main.java
+++ src/Exercicis/sapa/Main.java
@@ -3,11 +3,14 @@ package Exercicis.sapa;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("Hello students!");
+        System.out.println("Hello developers!");
         sayBye();
     }
-    public static void sayBye(){
+
+    public static void sayBye() {
         System.out.println("Bye, Bye");
+
     }
 }
