e3796fb9b009f04aee2526320406feb248af4cd4
AlexGrabulosaPuertas <a.grabulosa@sapalomera.cat>
2021-05-27 08:53:04 +0200

    funcion "saybye"

---
+++ src/Exercicis/sapa/Main.java
@@ -0,0 +1,13 @@
+package Exercicis.sapa;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello students!");
+        sayBye();
+    }
+    public static void sayBye(){
+        System.out.println("Bye, Bye");
+    }
+}
